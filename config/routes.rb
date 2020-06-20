cfc66ed834e5d2630f17500e824756be919c8f1f
8hachi8 <bold.ryst@gmail.com>
2020-06-20 17:14:26 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "reports#index"
+  # get "users#index"
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
 end
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
