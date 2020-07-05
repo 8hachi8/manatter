30620f62685176dcfe0eaa321cd83d3110dfd727
8hachi8 <bold.ryst@gmail.com>
2020-07-06 00:28:25 +0900

    Create header.js

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,24 @@
+$(function() {
+  $(".Navi__mypage").hover(function() {
+    $(this).css('background', '#0081c1');
+  }, function() {
+    $(this).css('background', '');
+  });
+
+
+  $(".Navi__inout").hover(function() {
+    $(this).css('background', '#0081c1');
+  }, function() {
+    $(this).css('background', '');
+  });
+
+});
+
+
+
+
+
+
+
+
+
