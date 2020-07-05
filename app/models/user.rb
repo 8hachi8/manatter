90f7735367617bec19936b050fa069f7530e67a9
8hachi8 <bold.ryst@gmail.com>
2020-07-06 00:16:25 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :group_users
   has_many :groups, through: :group_users
   has_many :messages
+  has_many :reports
+  has_many :comments
+
+  mount_uploader :image, ImageUploader
 end
