e5d9a061d2d471b6585ac18972006d6f079d9323
8hachi8 <bold.ryst@gmail.com>
2020-07-06 00:13:14 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   root "reports#index"
   devise_for :users
+  resources :reports do
+    collection do
+      get 'search'
+    end
+    resources :comments, only: :create
+  end
   resources :users, only: [:index, :show, :edit, :update]
   resources :groups, only: [:index, :new, :create,:edit, :update] do
     resources :messages, only: [:index, :create]
   end
 end
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
