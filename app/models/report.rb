d06ce1275d25d8a2b5272839d27d8ce0f4b18fcf
8hachi8 <bold.ryst@gmail.com>
2020-07-24 18:30:43 +0900

    Update report.rb

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,8 +2,10 @@ class Report < ApplicationRecord
   belongs_to :user
   # has_many :comments
 
-  def self.search(search)
-    return Report.all unless search
-    Report.where(['date LIKE (?) OR impression LIKE (?) OR user_id LIKE (?) OR job_id LIKE (?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
-  end
+  # def self.search(search)
+  #   return Report.all unless search
+  #   Report.where(['date LIKE (?) OR impression LIKE (?) OR user_id LIKE (?) OR job_id LIKE (?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
+  # end
+
+  # Report.
 end
