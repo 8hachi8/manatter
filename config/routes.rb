94bae84f3a5ec40008e716f4dfd945913dd88159
8hachi8 <bold.ryst@gmail.com>
2020-07-24 18:31:36 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,8 @@ Rails.application.routes.draw do
     end
     resources :comments, only: :create
   end
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
   resources :groups, only: [:index, :new, :create,:edit, :update] do
     resources :messages, only: [:index, :create]
   end
 end
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
