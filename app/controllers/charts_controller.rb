5e60c2eb1fb9526a49073604cff0aef5ffc569f9
8hachi8 <bold.ryst@gmail.com>
2020-07-24 18:30:36 +0900

    Delete charts_controller.rb

--- app/controllers/charts_controller.rb
+++
@@ -1,9 +0,0 @@
-class ChartsController < ApplicationController
-  def index
-    # @pie_chart = pie_chart Report.all.group(:user_id).count
-    # = pie_chart Report.all.group(:job_id).count
-    # = pie_chart Report.all.group(:hour_id).count
-    # = pie_chart Report.all.group(:date).count
-    
-  end
-end
